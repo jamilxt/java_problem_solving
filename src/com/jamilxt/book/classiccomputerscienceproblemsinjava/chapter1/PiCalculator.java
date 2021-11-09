0d767ac5da10384204d4cbc6977e2a187fa5530b
Jamilur Rahman <jamilur.rahman@bs-23.net>
2021-11-09 12:09:13 +0600

    add: 1.4 Calculating pi

---
+++ src/com/jamilxt/book/classiccomputerscienceproblemsinjava/chapter1/PiCalculator.java
@@ -0,0 +1,24 @@
+package com.jamilxt.book.classiccomputerscienceproblemsinjava.chapter1;
+
+/**
+ * @author jamilxt
+ * @created 09 Nov, 2021
+ */
+public class PiCalculator {
+    public static double calculatePi(int nTerms) {
+        final double numerator = 4.0;
+        double denominator = 1.0;
+        double operation =  1.0;
+        double pi = 0.0;
+        for (int i = 0; i < nTerms; i++) {
+            pi += operation * (numerator / denominator);
+            denominator += 2.0;
+            operation *= -1.0;
+        }
+        return pi;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculatePi(1000000));   // 3.1415916535897743
+    }
+}
